37ae002bcf2df45a16647e1e857579cf626c6af3
faustus85 <161504559+faustus85@users.noreply.github.com>
2025-02-02 22:18:00 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,17 @@
 terraform{
-    required_providers{
-      azurem={
-        source="hashicorp/azurerm"
-        version= ">=3.70.0"#this version azurem, not terraform version
-      }
+  required_providers{
+    azurerm={
+      source="hashicorp/azurerm"
+      version= ">=3.70.0"#this version is for azurerm, NOT terraform version
     }
-    required_version=">=1.4.0"#this version is for terraform version, not azurem
+  }
+  required_version=">=1.4.0"#this version is for Terraform Version, NOT azurerm
 }
 
-provider "azurem"{
-    features{}
-    subscription_id=var.subscription_id
-    client_id=var .client_id
-    client_secret=var.client_secret
-    tenant_id=var.tenant_id
+provider "azurerm"{
+  features{}  
+  subscription_id=var.subscription_id
+  client_id=var.client_id
+  client_secret=var.client_secret
+  tenant_id=var.tenant_id
 }
