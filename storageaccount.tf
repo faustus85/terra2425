2fe3673b0580b061ec83e6e1817977060291be15
faustus85 <161504559+faustus85@users.noreply.github.com>
2025-02-03 20:06:06 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,11 +1,1 @@
-resource "azurerm_storage_account" "mcitstoragejoe" {
-  name                     = "storageaccountjoe"
-  resource_group_name      = azurerm_resource_group.mcitjoseph24.name
-  location                 = azurerm_resource_group.mcitjoseph24.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
 
-  tags = {
-    environment = "staging"
-  }
-}
